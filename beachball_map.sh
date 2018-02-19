588c6aa5d22402679d1898ad794e44cc72d8ca47
Samuel Haugland <samhaug@umich.edu>
2018-02-19 17:34:30 -0500

    commit

--- beachball_map.sh
+++ beachball_map.sh
@@ -18,12 +18,11 @@ maxlat=47.0
 psbasemap -Y5i -R$region -J$scale -Ba2/a2/WSne -K -P > $file.ps
 pscoast -R$region -J$scale -A10 -Wthin,black -N1/thick,black -N2/thin,black \
 	-Slightblue -Di -K -O -P >> $file.ps
-psxy domain_box -R$region -J$scale -W6,black,- -K -O -P >> $file.ps
 
-while read lat lon H mrr mtt mpp mrt mrp mtp e lat lon; do
-psmeca -R -J -Sm0.4 -K -O -P << END >> $file.ps
+while read lat lon H mrr mtt mpp mrt mrp mtp e lon lat; do
+psmeca -R -J -Sm0.4 -K -O << END >> $file.ps
 lon lat dep stk dip rake mag
-$lon $lat $H $Stk $Dip $Rake $Mw
+$lon $lat $H $mrr $mtt $mpp $mrt $mrp $mrp $e $lon $lat
 END
 
 done < beachballs.dat
