740f74baacc502dc4a397d41cbc0f97069351fb7
Samuel McRae Haugland <samhaug@umich.edu>
2018-02-19 18:12:42 -0500

    ''

--- beachball_map.sh
+++ beachball_map.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
 region=80/150/-10/50 
-scale=m0.1i
+scale=m0.05i
 file=beachball_map
-gmtset LABEL_FONT_SIZE=8
-gmtset HEADER_FONT_SIZE=8
-gmtset ANNOT_FONT_SIZE_SECONDARY=8
-gmtset ANNOT_FONT_SIZE_PRIMARY=8
-
-minlon=246.0-360.0
-maxlon=253.0-360.0
-minlat=42.0
-maxlat=47.0
+gmtset FONT_LABEL=8
+gmtset FONT_TITLE=8
+gmtset FONT_ANNOT_PRIMARY=8
+gmtset FONT_ANNOT_SECONDARY=8
 
 #plot events
 psbasemap -Y5i -R$region -J$scale -B10g0.5 -K -P > $file.ps
-pscoast -R$region -J$scale -A10 -Wthin,black -N1/thick,black -N2/thin,black \
+pscoast -R$region -J$scale -Wthin,black -N1/thick,black -N2/thin,black \
 	-Slightblue -Di -K -O -P >> $file.ps
 
 while read lat lon H mrr mtt mpp mrt mrp mtp e lon lat; do
-psmeca -R$region -J -Sm0.4 -K -O << END >> $file.ps
+psmeca -R$region -J$scale -Sm0.4 -K -O << END >> $file.ps
 lon lat dep stk dip rake mag
 $lon $lat $H $mrr $mtt $mpp $mrt $mrp $mrp $e $lon $lat
 END
