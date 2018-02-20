d303bed5c16d7ccce4ac8b69bb783f13e519e38a
Samuel McRae Haugland <samhaug@umich.edu>
2018-02-19 19:13:01 -0500

    ''

--- beachball_map.sh
+++ beachball_map.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-region=80/150/-10/50 
+#region=80/150/-10/50 
+region=70/160/-20/60 
 scale=m0.1i
 file=beachball_map
 gmtset FONT_LABEL=8
@@ -8,15 +9,13 @@ gmtset FONT_ANNOT_PRIMARY=8
 gmtset FONT_ANNOT_SECONDARY=8
 
 #plot events
-psbasemap -Y5i -R$region -J$scale -B10 -K > $file.ps
+psbasemap -R$region -J$scale -B10 -K > $file.ps
 pscoast -R$region -J$scale -W -Slightblue -Dl -K -O  >> $file.ps
 
 while read lat lon H mrr mtt mpp mrt mrp mtp e lon lat; do
-psmeca -R$region -J$scale -Sm -K -O << END >> $file.ps
-lon lat dep stk dip rake mag
+psmeca -R$region -J$scale -Sm0.5 -K -O << END >> $file.ps
 $lon $lat $H $mrr $mtt $mpp $mrt $mrp $mrp $e $lon $lat
 END
-
 done < beachballs.dat
 
 ps2pdf $file.ps
