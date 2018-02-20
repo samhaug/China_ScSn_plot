e1e9663ca6a259160c5118ee999fd5fa37d98f74
Samuel Haugland <samhaug@umich.edu>
2018-02-20 10:48:52 -0500

    commit

---
+++ make_reflection_list.py
@@ -0,0 +1,63 @@
+#!/home/samhaug/anaconda2/bin/python
+
+'''
+==============================================================================
+
+File Name : make_reflection_list.py
+Purpose : make ascii files of reflection points for psxy
+Creation Date : 19-02-2018
+Last Modified : Tue 20 Feb 2018 10:47:57 AM EST
+Created By : Samuel M. Haugland
+
+==============================================================================
+'''
+
+import numpy as np
+from subprocess import call
+from os import listdir
+import argparse
+import h5py
+
+def main():
+    parser = argparse.ArgumentParser(description='make beachball list 4 psmeca')
+    parser.add_argument('-d','--dir_list',metavar='string',type=str,
+                       help='comma delimited list of direcories')
+    args = parser.parse_args()
+    dir_list = args.dir_list.split(',')
+    ScS2 = file('ScS2','w')
+    ScS3 = file('ScS3','w')
+    sScS = file('sScS','w')
+    sScS2 = file('sScS2','w')
+    sScS3 = file('sScS3','w')
+    for d in dir_list:
+        print d
+        phase_names,coord = reflection_points(d+'/3dreflection_points.h5')
+        for idx,ii in enumerate(phase_names):
+            if ii == 'sScS':
+                sScS.write(str(coord[idx][0])+' '+str(coord[idx][1])+'\n')
+            if ii == 'sScSScS':
+                sScS2.write(str(coord[idx][0])+' '+str(coord[idx][1])+'\n')
+            if ii == 'sScSScSScS':
+                sScS3.write(str(coord[idx][0])+' '+str(coord[idx][1])+'\n')
+            if ii == 'ScSScS':
+                ScS2.write(str(coord[idx][0])+' '+str(coord[idx][1])+'\n')
+            if ii == 'ScSScSScS':
+                ScS3.write(str(coord[idx][0])+' '+str(coord[idx][1])+'\n')
+    ScS2.close()
+    ScS3.close()
+    sScS.close()
+    sScS2.close()
+    sScS3.close()
+
+def reflection_points(fname):
+    f = h5py.File(fname,'r',driver='core')
+    phase_names = []
+    coord = []
+    for ikeys in f:
+        for phase in f[ikeys]:
+            if not phase.startswith('c'):
+                hkey = f[ikeys][phase].keys()[118]
+                phase_names.append(phase)
+                coord.append(f[ikeys][phase][hkey][:])
+    return phase_names,coord
+main()
---
+++ reflection_map.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+boxregion=80/150/-10/50 
+region=70/180/-30/60 
+#scale=m0.08i
+scale=m0.05i
+file=reflection_map
+#gmtset FONT_LABEL=8
+#gmtset FONT_TITLE=8
+#gmtset FONT_ANNOT_PRIMARY=8
+#gmtset FONT_ANNOT_SECONDARY=8
+
+#plot events
+#psbasemap -R$region -J$scale -B10 -K -D$boxregion -F > $file.ps
+#pscoast -R$region -J$scale -B10 -S#27A2B5 -G#B53A27 -K  > $file.ps
+pscoast -R$region -J$scale -B10 -Swhite -Glightgrey -K  > $file.ps
+
+while read lat lon H mrr mtt mpp mrt mrp mtp e lon lat; do
+psmeca -R$region -J$scale -Sm0.2i -K -O << END >> $file.ps
+$lon $lat $H $mrr $mtt $mpp $mrt $mrp $mrp $e $lon $lat
+END
+done < beachballs.dat
+
+ps2pdf $file.ps
+rm $file.ps
