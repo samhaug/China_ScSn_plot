bf21d6160865cf19c027fc3a95df1feb7a5b140d
Samuel McRae Haugland <samhaug@umich.edu>
2018-02-22 15:17:25 -0500

    commit

--- plot_raypaths.sh
+++ plot_raypaths.sh
@@ -11,15 +11,24 @@ region=50/130/3480/6371
 
 #S plot
 #gmt psxy CE_grid.dat -R$region3 -J$scale -B0 -Wthinnest,black -K -O -P >> $file.ps
-gmt psxy $datadir/sScS0.dat -R$region -J$scale -Bxa20 -Bya500 \
+#############################################################################
+gmt psxy $datadir/sScS0.dat -R$region -J$scale -Bxa20  \
                            -BwNsE -Wthin,black -Y20 -X0.1 -K -P > $file.ps
 gmt psxy $datadir/sScS1.dat -R$region -J$scale \
                            -BwNsE -Wthin,#27A2B5 -K -O >> $file.ps
 gmt psxy $datadir/sScS2.dat -R$region -J$scale \
                            -BwNsE -Wthin,#8127B5 -K -O >> $file.ps
 
-gmt psxy $datadir/sScSScS0.dat -R$region -J$scale -Bxa20 -Bya500 \
-                           -BwNsE -Wthin,black -X8 -K -O  >> $file.ps
+gmt psxy $datadir/670.dat -R$region -J$scale \
+                           -BwNsE -Wthinnest,black -K -O >> $file.ps
+gmt psxy $datadir/410.dat -R$region -J$scale \
+                           -Wthinnest,black -K -O >> $file.ps
+gmt psxy $datadir/220.dat -R$region -J$scale \
+                           -Wthinnest,black -K -O >> $file.ps
+
+#############################################################################
+gmt psxy $datadir/sScSScS0.dat -R$region -J$scale -Bxa20 \
+                           -BwNsE -Wthin,black -X6.5 -K -O  >> $file.ps
 gmt psxy $datadir/sScSScS1.dat -R$region -J$scale \
                            -BwNsE -Wthin,#27A2B5 -K -O >> $file.ps
 gmt psxy $datadir/sScSScS2.dat -R$region -J$scale \
@@ -27,8 +36,61 @@ gmt psxy $datadir/sScSScS2.dat -R$region -J$scale \
 gmt psxy $datadir/sScSScS3.dat -R$region -J$scale \
                            -BwNsE -Wthin,#B53A27 -K -O >> $file.ps
 
-#c -G#5BB527 
-#c -G#B5AF27 
+gmt psxy $datadir/670.dat -R$region -J$scale \
+                           -BwNsE -Wthinnest,black -K -O >> $file.ps
+gmt psxy $datadir/410.dat -R$region -J$scale \
+                           -Wthinnest,black -K -O >> $file.ps
+gmt psxy $datadir/220.dat -R$region -J$scale \
+                           -Wthinnest,black -K -O >> $file.ps
+
+#############################################################################
+gmt psxy $datadir/sScSScSScS0.dat -R$region -J$scale -Bxa20 \
+                           -BwNsE -Wthin,black -X6.5 -K -O  >> $file.ps
+gmt psxy $datadir/sScSScSScS1.dat -R$region -J$scale \
+                           -BwNsE -Wthin,#27A2B5 -K -O >> $file.ps
+gmt psxy $datadir/sScSScSScS2.dat -R$region -J$scale \
+                           -BwNsE -Wthin,#8127B5 -K -O >> $file.ps
+gmt psxy $datadir/sScSScSScS3.dat -R$region -J$scale \
+                           -BwNsE -Wthin,#B53A27 -K -O >> $file.ps
+gmt psxy $datadir/sScSScSScS4.dat -R$region -J$scale \
+                           -BwNsE -Wthin,#5BB527 -K -O >> $file.ps
+
+gmt psxy $datadir/670.dat -R$region -J$scale \
+                           -BwNsE -Wthinnest,black -K -O >> $file.ps
+gmt psxy $datadir/410.dat -R$region -J$scale \
+                           -Wthinnest,black -K -O >> $file.ps
+gmt psxy $datadir/220.dat -R$region -J$scale \
+                           -Wthinnest,black -K -O >> $file.ps
+
+#############################################################################
+gmt psxy $datadir/ScSScS0.dat -R$region -J$scale -Bxa180 \
+                           -BwNsE -Wthin,black -Y-3 -X-10 -K -O >> $file.ps
+gmt psxy $datadir/ScSScS1.dat -R$region -J$scale \
+                           -BwNsE -Wthin,#27A2B5 -K -O >> $file.ps
+
+gmt psxy $datadir/670.dat -R$region -J$scale \
+                           -BwNsE -Wthinnest,black -K -O >> $file.ps
+gmt psxy $datadir/410.dat -R$region -J$scale \
+                           -Wthinnest,black -K -O >> $file.ps
+gmt psxy $datadir/220.dat -R$region -J$scale \
+                           -Wthinnest,black -K -O >> $file.ps
+
+#############################################################################
+
+gmt psxy $datadir/ScSScSScS5.dat -R$region -J$scale -Bxa180 \
+                           -BwNsE -Wthin,black -X6.5 -K -O >> $file.ps
+gmt psxy $datadir/ScSScSScS1.dat -R$region -J$scale \
+                           -BwNsE -Wthin,#27A2B5 -K -O >> $file.ps
+gmt psxy $datadir/ScSScSScS2.dat -R$region -J$scale \
+                           -BwNsE -Wthin,#8127B5 -K -O >> $file.ps
+
+gmt psxy $datadir/670.dat -R$region -J$scale \
+                           -BwNsE -Wthinnest,black -K -O >> $file.ps
+gmt psxy $datadir/410.dat -R$region -J$scale \
+                           -Wthinnest,black -K -O >> $file.ps
+gmt psxy $datadir/220.dat -R$region -J$scale \
+                           -Wthinnest,black -K -O >> $file.ps
+#############################################################################
 
 ps2pdf $file.ps
 rm $file.ps
