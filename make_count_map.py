90117176d4ba8f4b8a068891c472392ceaab1168
Samuel Haugland <samhaug@umich.edu>
2018-04-03 12:55:57 -0400

    commit

--- make_count_map.py
+++ make_count_map.py
@@ -6,7 +6,7 @@
 File Name : make_count_map.py
 Purpose : plot spherical harmonics
 Creation Date : 17-02-2018
-Last Modified : Tue 03 Apr 2018 12:46:46 PM EDT
+Last Modified : Tue 03 Apr 2018 12:55:48 PM EDT
 Created By : Samuel M. Haugland
 
 ==============================================================================
@@ -34,8 +34,8 @@ def main():
     lon = f['lon'][:]
     xx,yy = np.meshgrid(lon,lat)
     gc = np.ravel(gc)
-    xx = np.ravel(xx)
-    yy = np.ravel(yy)
+    xx = np.transpose(np.ravel(xx))
+    yy = np.transpose(np.ravel(yy))
     np.savetxt('count_map.dat',np.c_[xx,yy,gc],fmt='%8.3f')
 
 main()
