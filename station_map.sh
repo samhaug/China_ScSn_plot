c9a828559a645c3d3a12846cd19957fe41fffaae
Samuel McRae Haugland <samhaug@umich.edu>
2018-08-24 16:04:07 -0400

    commit

--- station_map.sh
+++ station_map.sh
@@ -6,7 +6,7 @@ file=station_map
 china_stations=datfiles/stations.dat
 grid_stations=grid_stations.dat
 #stations=datfiles/obspydmt_statcoords.dat
-beach_file=datfiles/axisem3d_beachballs.dat
+beach_file=datfiles/beachballs.dat
 #gmtset FONT_LABEL=8
 
 #plot events
@@ -14,14 +14,21 @@ beach_file=datfiles/axisem3d_beachballs.dat
 #pscoast -R$region -J$scale -B10 -S#27A2B5 -G#B53A27 -K  > $file.ps
 pscoast -R$region -J$scale -Ba10 -Swhite -Glightgrey -A10000 -K  > $file.ps
 gmt psxy $grid_stations -R$region -J$scale -Si0.10c -Gblack -K -O >> $file.ps
+while read lat lon H mrr mtt mpp mrt mrp mtp e lon lat
+do
+psmeca -R$region -J$scale -Sm0.04i -K -O << EOF >> $file.ps
+$lon $lat $H $mrr $mtt $mpp $mrt $mrp $mtp $e $lon $lat
+EOF
+done < $beach_file
 
 pscoast -R$region -J$scale -Ba10 -Swhite -Glightgrey -A10000 -K -O -X4.5i >> $file.ps
 gmt psxy $china_stations -R$region -J$scale -Si0.10c -Gblack -K -O >> $file.ps
 
-while read lat lon H mrr mtt mpp mrt mrp mtp e lon lat; do
-psmeca -R$region -J$scale -Sm0.2i -K -O << END >> $file.ps
-$lon $lat $H $mrr $mtt $mpp $mrt $mrp $mrp $e $lon $lat
-END
+while read lat lon H mrr mtt mpp mrt mrp mtp e lon lat
+do
+psmeca -R$region -J$scale -Sm0.04i -K -O << EOF >> $file.ps
+$lon $lat $H $mrr $mtt $mpp $mrt $mrp $mtp $e $lon $lat
+EOF
 done < $beach_file
 #gmt psxy jeannot_1.dat -R$region -J$scale -W2,red -K -O >> $file.ps
 #gmt psxy jeannot_2.dat -R$region -J$scale -W2,blue -K -O >> $file.ps
@@ -30,3 +37,6 @@ ps2pdf $file.ps
 convert -density 150 $file.pdf -quality 90 $file.png
 rm $file.ps
 evince $file.pdf &> /dev/null
+
+
+
